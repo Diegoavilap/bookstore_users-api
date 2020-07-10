e21ef2c9db4e5259f4438b99fc9e251ab588e228
Diego Avila <diegoavilapava@hotmail.com>
2020-07-10 17:03:52 -0500

    Starting our domain, creating user domain and createUser method in controller

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -1,12 +1,28 @@
 package users
 
 import (
-"github.com/gin-gonic/gin"
-"net/http"
+	"github.com/diegoavilap/bookstore_users-api/domain/users"
+	"github.com/diegoavilap/bookstore_users-api/services"
+	"github.com/diegoavilap/bookstore_users-api/utils/errors"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func CreateUser(c *gin.Context){
-	c.String(http.StatusNotImplemented, "Implement me!")
+	var user users.User
+
+	if err := c.ShouldBindJSON(&user); err != nil{
+		restErr:= errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	result, saveError := services.CreateUser(user)
+	if saveError != nil {
+		c.JSON(saveError.Status, saveError)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context){
